122ebbeb4fb8c6af3eadb8ca599c5519cec5590b
jgrissom <jgrissom91@gmail.com>
2021-09-12 13:48:21 -0500

    [1.03] Create instance of Logger
    - Log sample messages for all log levels

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@
 
 
 using System;
+using NLog.Web;
+using System.IO;
 
 namespace nLogDemo
 {
@@ -11,7 +13,18 @@ namespace nLogDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+            // create instance of Logger
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+            // log sample messages
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
         }
     }
 }
