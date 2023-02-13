85b198b80137b05b04e147fe57723ee97fd0f93f
jgrissom <jgrissom91@gmail.com>
2023-02-13 10:05:57 -0600

    [1.06] Log an exception

--- Program.cs
+++ Program.cs
@@ -17,3 +17,15 @@ logger.Fatal("Sample fatal error message");
 // NLog supports structured messages
 var fruit = new[] { "bananas", "apples", "pears" };
 logger.Info("I like to eat {Fruit}", fruit);
+
+// Example of logging an exception
+try
+{
+    int x = 10;
+    int y = 0;
+    Console.WriteLine(x / y);
+}
+catch (Exception ex)
+{
+    logger.Error(ex.Message);
+}
